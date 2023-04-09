cc08570255c99ad5f9c3658806c9e03734756be3
Flavio Irani <firani6@gatech.edu>
2023-04-09 18:34:07 -0400

    Changed some files around, added draggable and card interfaces

---
+++ Assets/Card.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Card", menuName = "Card")]
+public class Card : ScriptableObject {
+
+    public new string name;
+    
+    public Sprite artwork;
+
+    public int atk;
+    public int def;
+    public int health;
+
+    public void cardEffect() {}
+
+}
